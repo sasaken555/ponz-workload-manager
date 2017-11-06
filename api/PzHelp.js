2dcdbd23096be3fd38a50275939592796215ebb5
PonzMild <accelerk@gmail.com>
2017-11-07 01:31:53 +0900

    #2 modify endpoint to /now -> /worknow

--- api/PzHelp.js
+++ api/PzHelp.js
@@ -1,5 +1,5 @@
 /*
- * PzTimeKeeper.js
+ * PzHelp.js
  * @author Sasaken555
  * @since 2017/11/05
  * 勤怠管理系APIを補助するその他のAPI群
@@ -7,7 +7,11 @@
 'use strict';
 
 module.exports = (app, options) => {
-  app.post('/now', (req, res, next) => {
+  app.post('/worknow', (req, res, next) => {
+    // Slackトークン認証を行う
+    // 認証エラーなら、403エラーで弾く!!
+
+    // 正常系
     res.status(200).send({
       response_type: 'in_channel',
       text: "現在時刻: " + new Date().toLocaleString()
